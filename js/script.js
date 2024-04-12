f25d8acf8cef0135757f176aff9f18a03a154b6e
Brian Wilson <brianwilsonnmanagement@gmail.com>
2024-04-12 18:13:04 +0100

    My first commit for kisa bank

---
+++ js/script.js
@@ -0,0 +1,6 @@
+setTimeout(function() {
+    var alertElement = document.querySelector('.alert');
+    alertElement.classList.remove('show');
+    alertElement.classList.add('hidden'); // Optional: Add a hide class for custom CSS animations or styling
+}, 7000);
+        
